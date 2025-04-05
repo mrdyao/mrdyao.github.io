ad8369d6082456baec5d313d75470060fdd4ebe1
mrdyao <mrdyao@outlook.com>
2025-04-05 20:22:47 +0800

    Deployed fbcddcb with MkDocs version: 1.6.1

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const logoLink = document.querySelector('.md-header__button.md-logo');
+    // 点击LOGO不跳转链接
     if (logoLink) {
         logoLink.addEventListener('click', function(event) {
             event.preventDefault();
         });
     }
 });
+
